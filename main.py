c5b6d2ed912a6eb45cce7675f08222e9d220064c
tarickali <tarickali97@gmail.com>
2026-03-06 11:53:45 -0500

    chore: clean up main.py

--- main.py
+++ main.py
@@ -10,18 +10,6 @@ from examples.circles import circles_driver
 def main():
     circles_driver()
 
-    # net = MLP([784, 512, 512, 10])
-    # x = Tensor(np.random.randn(1, 784))
-
-    # y = net(x)
-    # print(y.grad)
-    # print(x.grad)
-
-    # y.backward()
-
-    # print(y.grad)
-    # print(x.grad)
-
 
 if __name__ == "__main__":
     main()
